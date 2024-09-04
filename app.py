10dbb9a7b73b13039e41f93f744b52c153a33588
Соня Свиридова <idv251274@gmail.com>
2024-09-04 17:11:05 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return "<!doctype html>"\
+        "<html>"\
+        "     <body>"\
+        "         <h1>web-сервер на flask</h1>" \
+        "     </body>" \
+        "</html>"
+     
+    
