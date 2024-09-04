43082d2d23b5c61e3e4d84f15858059f120e0fd0
Соня Свиридова <idv251274@gmail.com>
2024-09-04 18:23:51 +0700

    Информация об авторе

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+
 @app.route("/web")
-def start():
+def web():
     return '''<!doctype html>
         <html>
             <body>
                 <h1>web-сервер на flask</h1>
+                <a href = "/author">author</a>
             </body> 
         </html>''' 
+
+@app.route("/author")
+def author():
+    name = "Свиридова Софья Денисовна"
+    group = "ФБИ-22"
+    faculty = "ФБ"
+
+    return '''<!doctype html>
+        <html>
+            <body>
+               <p>Студент: ''' + name + '''</p>
+               <p>Группа: ''' + group + '''</p>
+               <p>Факультет: ''' + faculty + '''</p>
+               <a href = "/web">web</a>
+            </body>
+        </html>'''    
      
     
