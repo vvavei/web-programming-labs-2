c481675c03151bf22a4a23232a929e539af898d8
Соня Свиридова <idv251274@gmail.com>
2024-09-13 18:23:05 +0700

    Стили

--- app.py
+++ app.py
@@ -37,9 +37,13 @@ def author():
 @app.route("/lab1/oak")
 def oak():
     path = url_for("static", filename="oak.jpg")
+    css_path = url_for("static", filename="lab1.css")
     return '''
 <!doctype html>
 <html>
+    <head>
+        <link rel = "stylesheet" href="''' + css_path + '''">
+    </head>
     <body>
         <h1>Дуб</h1>
         <img src = "''' + path + '''">
