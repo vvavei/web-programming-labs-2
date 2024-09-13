17ddb9a4a0a4e0fd99499d2cddab287b8f665722
Соня Свиридова <idv251274@gmail.com>
2024-09-13 18:34:39 +0700

    Исправление адресов

--- app.py
+++ app.py
@@ -5,20 +5,20 @@ app = Flask(__name__)
 def not_found(err):
     return "нет такой страницы", 404
 
-@app.route("/web")
+@app.route("/lab1/web")
 def web():
     return '''<!doctype html>
         <html>
             <body>
                 <h1>web-сервер на flask</h1>
-                <a href = "/author">author</a>
+                <a href = "/lab1/author">author</a>
             </body> 
         </html>''', 200, {
             'X-Server': 'sample',
             'Content-Type': 'text/plain; charset=utf-8'
         }
 
-@app.route("/author")
+@app.route("/lab1/author")
 def author():
     name = "Свиридова Софья Денисовна"
     group = "ФБИ-22"
@@ -30,7 +30,7 @@ def author():
                <p>Студент: ''' + name + '''</p>
                <p>Группа: ''' + group + '''</p>
                <p>Факультет: ''' + faculty + '''</p>
-               <a href = "/web">web</a>
+               <a href = "/lab1/web">web</a>
             </body>
         </html>'''    
 
@@ -80,9 +80,9 @@ def reset_counter():
 </html>
 '''
 
-@app.route("/info")
+@app.route("/lab1/info")
 def info():
-    return redirect("/author")
+    return redirect("/lab1/author")
 
 @app.route("/lab1/created")
 def created():
