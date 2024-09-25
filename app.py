5550ddba279d8a4daf772a852ee3ecaa96175fb7
Соня Свиридова <idv251274@gmail.com>
2024-09-25 14:43:55 +0700

    Фильтры

--- app.py
+++ app.py
@@ -434,3 +434,8 @@ def example():
 @app.route('/lab2/')
 def lab2():
     return render_template('lab2.html')
+
+@app.route('/lab2/filters')
+def filtres():
+    phrase = "О <b>сколько</b> <u>нам</u> <i>открытий</i> чудных..."
+    return render_template('filter.html', phrase = phrase)
