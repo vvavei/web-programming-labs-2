28cd940b2924441df501c281ff3cdbf91bdbf567
Соня Свиридова <idv251274@gmail.com>
2024-11-11 14:40:19 +0700

    Исправление SQL-инъекции

--- lab5.py
+++ lab5.py
@@ -38,13 +38,13 @@ def register():
     
     conn, cur = db_connect()
 
-    cur.execute(f"SELECT login FROM users WHERE login='{login}';")
+    cur.execute("SELECT login FROM users WHERE login=%s;", (login,))
     if cur.fetchone():
         db_close(conn,cur)
         return render_template('lab5/register.html', error = "Такой пользователь уже существует")
     
     password_hash = generate_password_hash(password)
-    cur.execute(f"INSERT INTO users (login,password) VALUES ('{login}', '{password_hash}');")
+    cur.execute("INSERT INTO users (login, password) VALUES (%s, %s);", (login, password_hash))
 
     db_close(conn, cur)
     return render_template('lab5/success.html', login =login)
@@ -62,7 +62,7 @@ def login():
     
     conn, cur = db_connect()
 
-    cur.execute(f"SELECT * FROM users WHERE login = '{login}';")
+    cur.execute("SELECT * FROM users WHERE login=%s;", (login,))
     user = cur.fetchone()
 
     if not user:
@@ -92,11 +92,12 @@ def create():
 
     conn, cur = db_connect()
 
-    cur.execute(f"SELECT * FROM users WHERE login=%s;", (login, ))
+    cur.execute("SELECT * FROM users WHERE login=%s;", (login,))
     user_id = cur.fetchone() ["id"]
 
-    cur.execute(f"INSERT INTO articles(user_id, title, article_text) \
-                VALUES ({user_id}, '{title}', '{article_text}');")
+    cur.execute("INSERT INTO articles (user_id, title, article_text) \
+                VALUES (%s, %s, %s);", (user_id, title, article_text))
+
 
     db_close(conn, cur)
     return redirect('/lab5')
@@ -110,10 +111,10 @@ def list():
     
     conn, cur = db_connect()
 
-    cur.execute(f"SELECT id FROM users WHERE login='{login}';")
+    cur.execute("SELECT id FROM users WHERE login=%s;", (login,))
     user_id = cur.fetchone() ["id"]
 
-    cur.execute(f"SELECT * FROM articles WHERE user_id ='{user_id}';")
+    cur.execute("SELECT * FROM articles WHERE user_id=%s;", (user_id,))
     articles = cur.fetchall()
 
     db_close(conn, cur)
