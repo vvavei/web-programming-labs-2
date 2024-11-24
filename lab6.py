0ab36faa0209df17659263512a3828b339a239b0
Соня Свиридова <idv251274@gmail.com>
2024-11-24 18:18:21 +0700

    Снятие аренды

--- lab6.py
+++ lab6.py
@@ -54,6 +54,36 @@ def api():
                     'id': id
                 }
             
+    if data['method'] == 'cancellation':
+        office_number = data['params']
+        for office in offices:
+            if office['number'] == office_number:
+                if office['tenant'] == '':
+                    return {
+                        'jsonrpc': '2.0',
+                        'error': {
+                            'code': 3,
+                            'message': 'Офис не арендован'
+                        },
+                        'id': id
+                    }
+                if office['tenant'] != login:
+                    return {
+                        'jsonrpc': '2.0',
+                        'error': {
+                            'code': 4,
+                            'message': 'Вы не можете снять чужую аренду'
+                        },
+                        'id': id
+                    }
+
+                office['tenant'] = ''
+                return {
+                    'jsonrpc': '2.0',
+                    'result': 'success',
+                    'id': id
+                }
+            
     return {
         'jsonrpc': '2.0',
         'error': {
