111595f388b926a3281c7581afc3a946e227bd71
Соня Свиридова <idv251274@gmail.com>
2024-12-05 10:38:43 +0700

    Получение информации о фильме/-ах по REST

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
+from lab7 import lab7
 
 app = Flask(__name__)
 
@@ -19,6 +20,7 @@ app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
+app.register_blueprint(lab7)
 
 
 @app.errorhandler(404)
