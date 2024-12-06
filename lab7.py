7c858cf355733b1102f324826834fa6921fb7507
Соня Свиридова <idv251274@gmail.com>
2024-12-06 19:06:31 +0700

    Редактирование выбранного фильма по REST

--- lab7.py
+++ lab7.py
@@ -78,3 +78,12 @@ def del_film(id):
      
     del films[id]
     return '', 204
+
+@lab7.route('/lab7/rest-api/films/<int:id>', methods=['PUT'])
+def put_film(id):
+    if id < 0 or id >= len(films):
+        return "Film not found", 404
+    
+    film = request.get_json()
+    films[id] = film
+    return films[id]
