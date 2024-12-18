5a9884a9d50bfd2571a2aa4b202df7800bc06fea
Соня Свиридова <idv251274@gmail.com>
2024-12-18 19:16:50 +0700

    Логаут

--- lab8.py
+++ lab8.py
@@ -3,7 +3,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 from db import db
 from db.models import users, articles
-from flask_login import login_user, login_required, current_user
+from flask_login import login_user, login_required, current_user, logout_user
 
 lab8 = Blueprint('lab8', __name__)
 
@@ -65,6 +65,12 @@ def login():
 @login_required
 def article_list():
     return "список статей"
+
+@lab8.route('/lab8/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect('/lab8/')
 
 
 
