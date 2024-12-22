45298983e0eba793894fafe793a0c0bd1cfb7a15
Соня Свиридова <idv251274@gmail.com>
2024-12-22 13:54:16 +0700

    Заготовка к 9-ой лабораторной

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
 from lab8 import lab8
+from lab9 import lab9
 
 app = Flask(__name__)
 
@@ -51,6 +52,7 @@ app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
 app.register_blueprint(lab8)
+app.register_blueprint(lab9)
 
 @app.errorhandler(404)
 def not_found(err):
@@ -110,6 +112,7 @@ def home():
                 <li><a href="/lab6/">Шестая лабораторная</a></li>
                 <li><a href="/lab7/">Седьмая лабораторная</a></li>
                 <li><a href="/lab8/">Восьмая лабораторная</a></li>
+                <li><a href="/lab9/">Девятая лабораторная</a></li>
             </ul>
         </div>
         <footer>
